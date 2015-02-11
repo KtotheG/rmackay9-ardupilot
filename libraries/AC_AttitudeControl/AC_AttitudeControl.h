5a5916f6ffa202fa70e10709c2eeb23e24210481
Leonard Hall <leonardthall@gmail.com>
2015-02-11 21:19:57 +0900

    AC_AttControl: get and save yaw-rate filter
    required for AutoTune for the yaw axis

--- libraries/AC_AttitudeControl/AC_AttitudeControl.h
+++ libraries/AC_AttitudeControl/AC_AttitudeControl.h
@@ -84,6 +84,12 @@ public:
     // set_dt - sets time delta in seconds for all controllers (i.e. 100hz = 0.01, 400hz = 0.0025)
     void set_rate_yaw_filt(float rate_yaw_filt);
 
+    // get_dt - sets time delta in seconds for all controllers (i.e. 100hz = 0.01, 400hz = 0.0025)
+    float get_rate_yaw_filt() const { return _rate_yaw_filt; }
+
+    // get_dt - sets time delta in seconds for all controllers (i.e. 100hz = 0.01, 400hz = 0.0025)
+    void save_rate_yaw_filt() const { _rate_yaw_filt.save(); }
+
     // relax_bf_rate_controller - ensure body-frame rate controller has zero errors to relax rate controller output
     void relax_bf_rate_controller();
 
